0351f9bd386814a692576809281f8bea14f09ddd
Bnag <uladzislau.samuseu@gmail.com>
2023-02-27 23:37:27 +0300

    added handlers GET ALL and POST articles

--- internal/app/api/handlers.go
+++ internal/app/api/handlers.go
@@ -1,3 +1,80 @@
 package api
 
 // Full API Handler initialization file
+
+import (
+	"encoding/json"
+	"net/http"
+	"simplewebserver/internal/app/models"
+)
+
+// Helper for generate messages
+type Message struct {
+	StatusCode int    `json:"status_code"`
+	Message    string `json:"message"`
+	IsError    bool   `json:"is_error"`
+}
+
+func initHandlers(writer http.ResponseWriter) {
+	writer.Header().Set("Content-type", "application/json")
+}
+
+// Return an actual list of all articles
+func (api *API) GetAllArticles(writer http.ResponseWriter, req *http.Request) {
+	// Headers initialization
+	initHandlers(writer)
+	// Logging the beginning of request processing
+	api.logger.Info("Get All Articles GET /api/v1/articles")
+	// Return anything from DB
+	articles, err := api.storage.Article().SelectAll()
+	if err != nil {
+		api.logger.Info("Error while Articles.SelectAll : ", err)
+		msg := Message{
+			StatusCode: 501,
+			Message:    "We have some troubles with accessing to DB",
+			IsError:    true,
+		}
+		writer.WriteHeader(501)
+		json.NewEncoder(writer).Encode(msg)
+		return
+	}
+	writer.WriteHeader(200)
+	json.NewEncoder(writer).Encode(articles)
+}
+
+func (api *API) PostArticle(writer http.ResponseWriter, req *http.Request) {
+	initHandlers(writer)
+	api.logger.Info("Post Article POST /api/v1/articles")
+	var article models.Article
+	err := json.NewDecoder(req.Body).Decode(&article)
+	if err != nil {
+		api.logger.Info("Invalid json recieved from client")
+		msg := Message{
+			StatusCode: 400,
+			Message:    "Provided json is invalid",
+			IsError:    true,
+		}
+		writer.WriteHeader(400)
+		json.NewEncoder(writer).Encode(msg)
+		return
+	}
+	a, err := api.storage.Article().Create(&article)
+	if err != nil {
+		api.logger.Info("Troubles while creating new article: ", err)
+		msg := Message{
+			StatusCode: 501,
+			Message:    "We have some troubles with accessing to DB",
+			IsError:    true,
+		}
+		writer.WriteHeader(501)
+		json.NewEncoder(writer).Encode(msg)
+	}
+	writer.WriteHeader(201)
+	json.NewEncoder(writer).Encode(a)
+}
+
+func (api *API) GetArticleById(writer http.ResponseWriter, req *http.Request) {}
+
+func (api *API) DeleteArticleById(writer http.ResponseWriter, req *http.Request) {}
+
+func (api *API) PostUserRegister(writer http.ResponseWriter, req *http.Request) {}
